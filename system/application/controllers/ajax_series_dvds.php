0a420c9da193943e083158e678187537fa94a857
Steve Dibb <beandog@gentoo.org>
2011-04-20 22:56:15 -0600

    Allow the model to set zero

--- system/application/controllers/ajax_series_dvds.php
+++ system/application/controllers/ajax_series_dvds.php
@@ -13,9 +13,6 @@
 			$series_dvds_id = $this->input->get_post('series_dvds_id');
 			$ix = intval($this->input->get_post('ix'));
 			
-			if(!$ix)
-				$ix = pg_null($ix);
-		
 			$this->series_dvds_model->load($series_dvds_id);
 			$this->series_dvds_model->set_ix($ix);
 		
@@ -26,9 +23,6 @@
 			$series_dvds_id = $this->input->get_post('series_dvds_id');
 			$season = intval($this->input->get_post('season'));
 			
-			if(!$season)
-				$season = pg_null($season);
-		
 			$this->series_dvds_model->load($series_dvds_id);
 			$this->series_dvds_model->set_season($season);
 		
@@ -49,9 +43,6 @@
 			$series_dvds_id = $this->input->get_post('series_dvds_id');
 			$volume = intval($this->input->get_post('volume'));
 			
-			if(!$volume)
-				$volume = pg_null($volume);
-		
 			$this->series_dvds_model->load($series_dvds_id);
 			$this->series_dvds_model->set_volume($volume);
 		
