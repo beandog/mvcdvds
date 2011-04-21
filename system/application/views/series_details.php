51d63ae6c8b36d67e5ddf1471052eefb145c504f
Steve Dibb <beandog@gentoo.org>
2011-04-20 23:08:54 -0600

    Don't display zero for average length; Display title

--- system/application/views/series_details.php
+++ system/application/views/series_details.php
@@ -9,20 +9,22 @@
 	extract($series);
 
 	echo form_open("series/update/$id", $attr);
+	
+	$display_average_length = ($average_length ? $average_length : "");
 
 	$input_collection = form_dropdown('collection', $collections, $collection['id']);
 	$input_preset = form_dropdown('preset_id', $presets, $preset['id']);
-	$input_long_title = form_input('title_long', $title_long, "size='50'");
 	$input_title = form_input('title', $title, "size='50'");
-	$input_average_length = form_input('average_length', $average_length, "size='3'");
+	$input_long_title = form_input('title_long', $title_long, "size='50'");
+	$input_average_length = form_input('average_length', $display_average_length, "size='3'");
 	$input_production_year = form_input('production_year', $production_year, "size='4'");
 	$input_indexed = form_checkbox('indexed', 'accept', pg_bool($indexed));
 	$input_grayscale = form_checkbox('grayscale', 'accept', pg_bool($grayscale));
 	
 	$this->table->add_row(array("Collection:", $input_collection));
 	$this->table->add_row(array("Preset:", $input_preset));
+	$this->table->add_row(array("Display Title:", $input_title));
 	$this->table->add_row(array("Long Title:", $input_long_title));
-	$this->table->add_row(array("Sorting Title:", $input_title));
 	$this->table->add_row(array("Avg. Length:", $input_average_length));
 	$this->table->add_row(array("Production Year:", $input_production_year));
 	$this->table->add_row(array("Indexed:", $input_indexed));
