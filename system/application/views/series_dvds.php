6ba45f6f30ba9c4f86e9b5ec30e05e76d0ca9cbb
Steve Dibb <steve.dibb@gmail.com>
2013-07-04 11:34:32 -0600

    Display filesizes; display totals

--- system/application/views/series_dvds.php
+++ system/application/views/series_dvds.php
@@ -11,16 +11,44 @@
 		'Disc',
 		'Side',
 		'Eps.',
+		'Filesize',
 	
 	);
 	
 	$this->table->set_heading($heading);
+
+	$arr_seasons = array();
+	$arr_volumes = array();
+	$total_seasons = null;
+	$total_volumes = null;
+	$total_discs = 0;
+	$total_episodes = 0;
+	$total_filesize = 0;
 	
 	foreach($dvds as $id => $row) {
-	
+
 		extract($row);
+
+		$total_discs++;
+		if($filesize)
+			$total_filesize += $filesize;
 		
 		$num_episodes = count($episodes[$id]);
+
+		if(intval($season)) {
+			$arr_seasons[] = $season;
+			$arr_seasons = array_unique($arr_seasons);
+			$total_seasons = count($arr_seasons);
+		}
+
+		if(intval($volume)) {
+			$arr_volumes[] = $volume;
+			$arr_volumes = array_unique($arr_volumes);
+			$total_volumes = count($arr_volumes);
+		}
+
+		if(intval($num_episodes))
+			$total_episodes += $num_episodes;
 		
 		if(!$num_episodes)
 			$num_episodes = "";
@@ -48,6 +76,7 @@
 		$display_season = ($season ? $season : "");
 		$display_volume = ($volume ? $volume : "");
 		$display_ix = ($ix ? $ix : "");
+		$display_filesize = number_format($filesize)." MB";
 		
 		$a_dvd2 = anchor("dvds/details/$id", $img_dvd);
 		$display_season = "<span>$display_season</span>";
@@ -55,6 +84,7 @@
 		$display_ix = "<span>$display_ix</span>";
 		$display_side = "<span>$side</span>";
 		$display_num_episodes = "<span>$num_episodes</span>";
+		$display_filesize = "<span>$display_filesize</span>";
 		
 		$tbl_row = array(
 			$a_dvd2,
@@ -64,11 +94,26 @@
 			$display_ix,
 			$display_side,
 			$display_num_episodes,
+			$display_filesize,
 		);
 		
 		$this->table->add_row($tbl_row);
 	
 	}
+
+	// Add totals
+	$totals_row = array(
+		'',
+		"<b>Totals</b>",
+		"<b>$total_seasons</b>",
+		"<b>$total_volumes</b>",
+		"<b>$total_discs</b>",
+		'',
+		"<b>$total_episodes</b>",
+		"<b>".number_format($total_filesize). " MB</b>",
+	);
+
+	$this->table->add_row($totals_row);
 	
 	$tmpl = array(
 		'table_open' => '<table border="0" cellpadding="4" cellspacing="0" class="zebra">',
