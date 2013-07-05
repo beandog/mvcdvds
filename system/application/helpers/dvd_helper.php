e51c72132cb8335b9898177468868045e6a7d683
Steve Dibb <steve.dibb@gmail.com>
2013-07-04 18:38:55 -0600

    Pad the averages so that within 60 seconds more or less of average length will be considered close.

--- system/application/helpers/dvd_helper.php
+++ system/application/helpers/dvd_helper.php
@@ -13,9 +13,13 @@
 	
  		$average = $average * 60;
 		
-		$max = $average + ($average * ($slide / 100));
-		$min = $average - ($average * ($slide / 100));
-		
+		// The +/- 60 is to pad the average by one minute,
+		// so if something is within 120 seconds of it, it's probably valid.
+		// Fex: if average is 7 mins, then 6 and 8 mins will
+		// immediately fall in the range
+		$max = $average + 60 + ($average * ($slide / 100));
+		$min = $average - 60 - ($average * ($slide / 100));
+
 		if(($length < $max) && ($length > $min))
 			return true;
 		else
