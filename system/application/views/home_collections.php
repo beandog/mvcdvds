b85942f6ae8227481a29d797a7734c71e917acd1
Steve Dibb <steve.dibb@gmail.com>
2014-07-23 11:17:59 -0600

    Fix search URL

--- system/application/views/home_collections.php
+++ system/application/views/home_collections.php
@@ -13,7 +13,7 @@
 			</ul>
 		</td>
 		<td valign='top'>
-			<form action='index.php/welcome/search' method='post'>
+			<form action='<?=$this->config->item('base_url').$this->config->item('index_page');?>/welcome/search' method='post'>
 				<input type='text' name='q'> <input type='submit' value='Search'>
 			</form>
 		</td>
