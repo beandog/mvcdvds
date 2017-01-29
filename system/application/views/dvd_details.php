574392de5326e71d9f88a9d7e7feedee56915509
Steve Dibb <steve.dibb@gmail.com>
2017-01-29 15:01:51 -0700

    Remove support for disabling dvdnav

--- system/application/views/dvd_details.php
+++ system/application/views/dvd_details.php
@@ -18,14 +18,14 @@
 	if(!isset($series_dvd)) {
 		$series_dvd['series_id'] = null;
 		$series_dvd['audio_preference'] = null;
-		$series_dvd['no_dvdnav'] = null;
+	// 	$series_dvd['no_dvdnav'] = null;
 	}
 
 	echo form_open("dvds/update_series_dvd/".$dvds['id'], $attr);
 
 	$i_series_id = form_dropdown('series_id', $select_series, $series_dvd['series_id']);
 	$i_audio_preference = form_dropdown('audio_preference', $select_audio_preference, $series_dvd['audio_preference']);
-	$i_no_dvdnav = form_checkbox('no_dvdnav', 't', $series_dvd['no_dvdnav'] == 't');
+	// $i_no_dvdnav = form_checkbox('no_dvdnav', 't', $series_dvd['no_dvdnav'] == 't');
 
 	if(!empty($title))
 		$this->table->add_row(array("Title:", $title));
@@ -38,7 +38,7 @@
 		$this->table->add_row(array("Longest Track:", $longest_track));
 	$this->table->add_row(array("Series:", $i_series_id));
 	$this->table->add_row(array("Audio:", $i_audio_preference));
-	$this->table->add_row(array("No dvdnav:", $i_no_dvdnav));
+	// $this->table->add_row(array("No dvdnav:", $i_no_dvdnav));
 
 	$submit = form_submit('submit', 'Update');
 
