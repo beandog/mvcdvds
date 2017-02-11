55057030471e920525331d6cd641d9848b015558
Steve Dibb <steve.dibb@gmail.com>
2017-02-11 16:09:00 -0700

    Force to a boolean value

--- system/application/controllers/series.php
+++ system/application/controllers/series.php
@@ -101,7 +101,7 @@
 				'average_length' => intval($this->input->post('average_length')),
 				'production_year' => $this->input->post('production_year'),
 				'indexed' => bool_pg($this->input->post('indexed')),
-				'grayscale' => intval($this->input->post('grayscale')),
+				'grayscale' => (bool)($this->input->post('grayscale')),
 			);
 
 			$this->series_model->set($arr);
