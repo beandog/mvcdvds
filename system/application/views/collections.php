bd6b9146b5c718dd89174c21593df040e259af15
Steve Dibb <steve.dibb@gmail.com>
2017-04-07 16:12:47 -0600

    Update display, Plex filesize

--- system/application/views/collections.php
+++ system/application/views/collections.php
@@ -11,10 +11,12 @@
 		'Plex',
 //		'Total Filesize',
 		'Preset',
-		'Prod. Year',
+		// 'Prod. Year',
 //		'Preset Filesize',
-		'QA',
-		'Missing Metadata',
+		'Filesize',
+		'Episodes',
+		// 'QA',
+		// 'Missing Metadata',
 // 		'# Discs',
 // 		'# Seasons',
 // 		'# Volumes',
@@ -29,6 +31,9 @@
 	$this->table->set_heading($header);
 
 	$total_filesize = 0;
+	$total_isos_filesize = 0;
+	$total_episodes_filesize = 0;
+	$total_series_episodes_filesize = 0;
 	$total_preset_filesize = 0;
 	$total_num_dvds = 0;
 	$total_num_episodes = 0;
@@ -40,7 +45,17 @@
 
 		extract($row);
 
-		$num_plex = count(preg_grep("/^$id\.0*${series_id}\..+\.mp4$/", $plex_collection_files));
+		$plex_episodes = preg_grep("/^$id\.0*${series_id}\..+\.mp4$/", $plex_collection_files);
+		$num_plex = count($plex_episodes);
+
+		$total_series_episodes_filesize = 0;
+		foreach($plex_episodes as $plex_episode) {
+			$episode_filesize = filesize("/opt/plex/episodes/$plex_episode");
+			$total_series_episodes_filesize += $episode_filesize; 
+
+		}
+
+		$total_episodes_filesize += $total_series_episodes_filesize;
 
 		if(count($metadata[$series_id])) {
 			$class = 'update';
@@ -74,6 +89,8 @@
 
 		$d_preset_filesize = "&nbsp; ".number_format($series_numbers[$series_id]['megabytes'])." MB";
 
+		$d_total_series_episodes_filesize = "&nbsp; ". number_format($total_series_episodes_filesize / (1024 * 1024))." MB";
+
 		$d_qa = '';
 		if($qa_notes)
 			$d_qa = 'Yes';
@@ -96,10 +113,12 @@
 			$d_num_plex,
 			// $d_filesize,
 			$d_preset,
-			$d_production_year,
-			$d_qa,
+			$d_filesize,
+			// $d_production_year,
+			// $d_qa,
 			// $d_preset_filesize,
-			$d_missing_metadata,
+			$d_total_series_episodes_filesize,
+			// $d_missing_metadata,
 		);
 
 		$this->table->add_row($table_row);
@@ -116,6 +135,7 @@
 	$display_total_episodes = number_format($total_num_episodes);
 	$display_total_plex = number_format($total_num_plex);
 	$display_total_filesize = number_format($total_filesize). " MB";
+	$display_total_episodes_filesize = number_format($total_episodes_filesize / (1024 * 1024)). " MB";
 	$display_total_preset_filesize = number_format($total_preset_filesize). " MB";
 
 	// Display totals
@@ -126,12 +146,9 @@
 		$display_total_dvds,
 		$display_total_episodes,
 		$display_total_plex,
-		// $display_total_filesize,
 		'',
-		'',
-		'',
-		// $display_total_preset_filesize,
-		'',
+		$display_total_filesize,
+		$display_total_episodes_filesize,
 	));
 
 	echo $this->table->generate();
