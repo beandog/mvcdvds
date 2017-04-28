e8f7afc80dc66c5dd2a125ac99f39021ea7f1b90
Steve Dibb <steve.dibb@gmail.com>
2017-04-28 12:44:04 -0600

    Add index for Blu-rays

--- system/application/views/html_title.php
+++ system/application/views/html_title.php
@@ -13,6 +13,7 @@
 	echo " | ".anchor("collections/index/4", "Movies");
 	echo " | ".anchor("collections/index/5", "Missing");
 	echo " | ".anchor("collections/index/6", "Box Sets");
+	echo " | ".anchor("collections/index/8", "Blu-rays");
 	echo "</td><td align='right'>";
 ?>
 <form action='<?=$this->config->item('base_url').$this->config->item('index_page');?>/welcome/search' method='post'><input type='text' name='q'> <input type='submit' value='Search'></form></td>
