c5aca722a864e16a13ea70e6722ce9a54c14ac79
Steve Dibb <steve.dibb@gmail.com>
2017-09-09 19:53:25 -0600

    Re-arrange year and average length

--- system/application/views/series_details.php
+++ system/application/views/series_details.php
@@ -19,8 +19,8 @@
 	$input_title = form_input('title', $title, "size='50'");
 	$input_collection = form_dropdown('collection', $collections, $collection['id']);
 	$input_preset = form_dropdown('preset_id', $arr_dropdown_presets, $preset['id']);
-	$input_average_length = form_input('average_length', $display_average_length, "size='3'");
 	$input_production_year = form_input('production_year', $production_year, "size='4'");
+	$input_average_length = form_input('average_length', $display_average_length, "size='3'");
 	$input_indexed = form_checkbox('indexed', 'accept', pg_bool($indexed));
 	$input_grayscale = form_checkbox('grayscale', 'accept', $grayscale);
 	$input_qa_notes = form_textarea('qa_notes', $qa_notes);
@@ -29,8 +29,8 @@
 	$this->table->add_row(array("Display Title:", $input_title));
 	$this->table->add_row(array("Collection:", $input_collection));
 	$this->table->add_row(array("Preset:", $input_preset));
-	$this->table->add_row(array("Avg. Length:", $input_average_length));
 	$this->table->add_row(array("Production Year:", $input_production_year));
+	$this->table->add_row(array("Avg. Length:", $input_average_length));
 	$this->table->add_row(array("Indexed:", $input_indexed));
 	$this->table->add_row(array("Grayscale:", $input_grayscale));
 	$this->table->add_row(array("QA Notes:", $input_qa_notes));
