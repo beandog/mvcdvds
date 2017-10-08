29a589c469a19e085fe476fcc38afaa345e3a8ca
Steve Dibb <steve.dibb@gmail.com>
2017-10-08 13:13:20 -0600

    Add header for documentaries

--- system/application/views/html_title.php
+++ system/application/views/html_title.php
@@ -13,6 +13,7 @@
 	echo " | ".anchor("collections/index/4", "Movies");
 	echo " | ".anchor("collections/index/5", "Missing");
 	echo " | ".anchor("collections/index/6", "Box Sets");
+	echo " | ".anchor("collections/index/7", "Documentary");
 	echo " | ".anchor("collections/index/8", "Blu-rays");
 	echo "</td><td align='right'>";
 ?>
