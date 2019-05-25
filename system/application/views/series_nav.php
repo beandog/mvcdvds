dea9511398c9fc76768f24a3ae23d75df271bd48
Steve Dibb <steve.dibb@gmail.com>
2019-05-25 14:29:06 -0600

    Display disc title, season, volume, side in header

--- system/application/views/series_nav.php
+++ system/application/views/series_nav.php
@@ -2,6 +2,19 @@
 
 	echo heading($series['title']." - ".$series['nsix'], 2);
 
+	if(isset($series_dvds)) {
+		$display_season = array();
+		if(strlen($series_dvds[$dvd_id]['title']))
+			$display_season[] = $series_dvds[$dvd_id]['title'];
+		$display_season[] = "Season: ".str_pad($series_dvds[$dvd_id]['season'], 2);
+		if($series_dvds[$dvd_id]['volume'])
+			$display_season[] = "Volume: ".str_pad($series_dvds[$dvd_id]['volume'], 2);
+		$display_season[] = "Disc: ".str_pad($series_dvds[$dvd_id]['ix'], 2);
+		if(strlen(trim($series_dvds[$dvd_id]['side'])))
+			$display_season[] = "Side: ".str_pad($series_dvds[$dvd_id]['side'], 2);
+		$display_season_str = implode(' | ', $display_season);
+	}
+
  	$a_dvds = anchor("series/dvds/".$series['id'], "DVDs");
  	$a_tracks = anchor("dvds/tracks/$dvd_id", "Tracks");
  	$a_episodes = anchor("dvds/episodes/$dvd_id", "Episodes");
@@ -20,7 +33,9 @@
 		$img = "";
 
  	echo "$a_dvds | $a_tracks | $a_episodes | $a_details | $a_series";
-	if(strlen($preset_name))
-		echo " - $preset_name";
+	// if(strlen($preset_name))
+	//	echo " | Preset: $preset_name";
+	if(isset($series_dvds))
+		echo " | $display_season_str";
 	echo "&nbsp; $img";
 
