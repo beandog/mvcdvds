8920bf9e6bd37223b37c9173746948aa9c52c0a4
Steve Dibb <steve.dibb@gmail.com>
2022-08-07 00:51:59 -0600

    Remove unused 'unarchived series' in collections

--- system/application/models/collections_model.php
+++ system/application/models/collections_model.php
@@ -46,20 +46,4 @@
 
 		}
 
-		public function get_unarchived_series($collection_id) {
-
-			$this->db->select('series.id, series.title');
-			$this->db->join('series_dvds', 'series_dvds.series_id = series.id');
-			$this->db->join('dvds', 'dvds.id = series_dvds.dvd_id');
-			$this->db->where('series.collection_id', $collection_id);
-			$this->db->where('dvds.longest_track', null);
-			$this->db->order_by('series.title');
-			$this->db->distinct();
-
-			$arr = $this->get_all('series');
-
-			return $arr;
-
-		}
-
 	}
