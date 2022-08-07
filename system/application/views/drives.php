80cea85d7fee80f170db04de70cda7614b86e6bd
Steve Dibb <steve.dibb@gmail.com>
2022-08-07 00:22:49 -0600

    Remove old drives code

--- system/application/views/drives.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-	echo heading("Disc Drives");
-
-	$arr[] = anchor("#", "Import DVD", "onclick='import_dvd(); return false'");
-	$arr[] = anchor("#", "Eject", "onclick='eject(); return false'");
-
-	echo ul($arr);
