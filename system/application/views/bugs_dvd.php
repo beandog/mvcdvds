bac3a84383401c5f7693feff74aced44dc32097c
Steve Dibb <steve.dibb@gmail.com>
2023-07-03 03:54:49 -0600

    Remove old

--- system/application/views/bugs_dvd.php
+++ system/application/views/bugs_dvd.php
@@ -66,30 +66,3 @@
 
 	echo "</blockquote>";
 
-	/*
-	$attr = array('id' => 'bugs_update', 'autocomplete' => 'off');
-
-
-	echo form_open("dvds/update_bugs/".$dvds['id'], $attr);
-
-	foreach($bugs as $dvd_bug) {
-
-		$checked = false;
-		if($dvd_bug['dvd_id'])
-			$checked = true;
-		$input_bugs[$dvd_bug['id']] = form_checkbox("dvd_bug[".$dvd_bug['id']."]", 'accept', $checked);
-		$this->table->add_row(array($input_bugs[$dvd_bug['id']], $dvd_bug['name']));
-
-	}
-
-	$submit = form_submit('submit', 'Update');
-
-	echo $this->table->generate();
-	$this->table->clear();
-
-	echo "<p>$submit</p>";
-
-	echo form_close();
-
-	echo "</blockquote>";
-	*/
