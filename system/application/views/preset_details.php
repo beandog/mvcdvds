8e0e439a0c3ed13389f4a6abc000e3fb7059858d
Steve Dibb <steve.dibb@gmail.com>
2025-03-08 01:16:38 -0700

    Update audio codecs

--- system/application/views/preset_details.php
+++ system/application/views/preset_details.php
@@ -13,7 +13,7 @@
 	$inputs = array();
 
 	// $o_vcodec = array('x264' => 'avc', 'x265' => 'hevc');
-	$o_acodec = array('copy' => 'copy', 'mp3' => 'lame');
+	$o_acodec = array('copy' => 'copy', 'fdk_aac' => 'aac', 'mp3' => 'lame', 'flac' => 'flac');
 	// $o_x264_preset = array('medium' => 'medium', 'slow' => 'slow');
 	$o_x264_tune = array('' => '', 'film' => 'film', 'animation' => 'animation');
 	// $o_fps = array('' => '', '23.976' => '23.97', '24' => '24', '25' => '25', '29.97' => '29.97', '30' => '30', '60' => '60');
