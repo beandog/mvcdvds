b0e743612695e5f3ce4aba4663d8d51dd21e5871
Steve Dibb <steve.dibb@gmail.com>
2025-04-24 20:49:56 -0600

    Display last mtime

--- system/application/views/episode_details.php
+++ system/application/views/episode_details.php
@@ -2,6 +2,8 @@
 
 	$play_filename = str_replace('/opt', '', $filename);
 	$episode_filename = basename($filename);
+	$episode_mtime = filectime($filename);
+	$d_mtime = date("Y-m-d", filectime($filename));
 
 	$d_play = "<img src='/images/icons/control_play_blue.png' onclick=\"play_episode('$play_filename', '$episode_filename');\">";
 
@@ -9,5 +11,6 @@
 	echo heading($episode_filename, 4);
 	echo "<video controls height='480' id='video' hidden><source src='' type='video/mp4' id='episode_player'></video>";
 	echo "<blockquote>\n";
+	echo "<pre style='font-size: 16px;'>Last modified: $d_mtime</pre>";
 	echo "<pre style='font-size: 16px;'>$mediainfo</pre>";
 	echo "</blockquote>\n";
