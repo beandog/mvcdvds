0f1b4afbd9900f8dc85aecfe4466e2ea01e5f11c
Steve Dibb <steve.dibb@gmail.com>
2026-09-21 16:47:59 -0600

    Remove setting vcodec

--- application/controllers/Presets.php
+++ application/controllers/Presets.php
@@ -48,7 +48,6 @@
 				$arr['name'] = $this->input->post('name');
 				$arr['x264_tune'] = $this->input->post('x264_tune');
 				$arr['crf'] = $crf;
-				$arr['vcodec'] = $this->input->post('vcodec');
 				$arr['acodec'] = $acodec;
 
 				$this->presets_model->set($arr);
